37f9cbcd1d7c77a103353e976a65a394489725c7
Oleksii Dubok <alexd2126@gmail.com>
2017-06-30 22:45:11 +0300

    Remove extra comments from devserver

--- devserver.js
+++ devserver.js
@@ -7,19 +7,12 @@ let path             = require('path');
 
 new WebpackDevServer(webpack(config), {
     contentBase       : path.join(__dirname, 'dist'),
-    // compress          : true,
     port              : 3000,
-    // stats             : 'errors-only',
     stats: { colors: true },
     open              : true,
     historyApiFallback: true,
     hot               : true,
     hotOnly: true
-    // headers: {
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    //     "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    // }
 }).listen(3000, '0.0.0.0', (err) => {
     if (err) {
         return console.log(err);
