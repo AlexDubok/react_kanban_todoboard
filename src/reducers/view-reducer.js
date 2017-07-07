dd45ca85e081cea9f20ad4398c40633b8b9bf361
Oleksii Dubok <alexd2126@gmail.com>
2017-07-07 21:42:14 +0300

    implement view reducer

--- src/reducers/view-reducer.js
+++ src/reducers/view-reducer.js
@@ -1,14 +1,25 @@
+import { SET_EDITING_STATUS, SEARCH_BY_NAME, EDIT_TASK } from '../actions/view';
 
 const defaultState =    {
-    dragging   : null,
-    editingTask: null
+    searchQuery  : '',
+    dragging     : null,
+    editingStatus: '',
+    editingTask  : {
+        name       : '',
+        description: '',
+        status     : 'todo'
+    }
 };
 
 export default function (state = defaultState, action) {
     switch (action.type) {
         case 'DRAG_TASK':
             return { ...state, dragging: action.id };
-        case 'EDIT_TASK':
+        case SET_EDITING_STATUS :
+            return { ...state, editingStatus: action.item };
+        case SEARCH_BY_NAME :
+            return { ...state, searchQuery: action.query };
+        case EDIT_TASK :
             return { ...state, editingTask: action.task };
         default:
             return state;
